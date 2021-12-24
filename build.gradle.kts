e424d4e665ab7f2a21a3d7a955a46bad3398ce97
Huynh, Nhan <nhan.huynh@stud.tu-darmstadt.de>
2021-12-24 13:10:06 +0100

    Add grader-info for graderJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 plugins {
   java
 }
+
+tasks {
+  create<Jar>("graderJar") {
+    group = "build"
+    afterEvaluate {
+      archiveFileName.set("FOP-2022-H09-${project.version}.jar")
+      from(project(":grader").sourceSets.main.get().allSource)
+      from(project(":solution").sourceSets.main.get().allSource)
+      from(project(":solution").sourceSets.test.get().allSource)
+    }
+  }
+}
+
 allprojects {
   apply(plugin = "java")
   repositories {
