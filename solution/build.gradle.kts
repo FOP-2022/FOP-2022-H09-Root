b0ae5b86020c3e76df4e6d3f7d9923eb3bcc7f47
Nyanyan <46193012+zentox@users.noreply.github.com>
2022-01-02 17:39:08 +0100

    Add submitter plugin

--- solution/build.gradle.kts
+++ solution/build.gradle.kts
@@ -1,9 +1,23 @@
+import org.sourcegrade.submitter.submit
+
+plugins {
+  id("org.sourcegrade.submitter") version "0.4.0"
+}
+
 dependencies {
   // JUnit only available in "test" source set (./src/test)
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
+
+
 tasks {
+  submit {
+    assignmentId = "h04"
+    studentId = "ab12cdef"
+    firstName = "sol_first"
+    lastName = "sol_last"
+  }
   test {
     useJUnitPlatform()
   }
