1bfe0eea6f0f7e1b319bdfa7b76d4858197f8619
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-11 20:45:27 +0100

    2 -> 4 spaces and add style plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,36 @@
 plugins {
-  java
+    java
+    id("org.sourcegrade.style") version "1.1.0"
 }
+
+tasks {
+    create<Jar>("graderJar") {
+        group = "build"
+        afterEvaluate {
+            archiveFileName.set("FOP-2022-H09-${project.version}.jar")
+            from(project(":grader").sourceSets.main.get().allSource)
+            from(project(":solution").sourceSets.main.get().allSource)
+            from(project(":solution").sourceSets.test.get().allSource)
+        }
+    }
+}
+
 allprojects {
-  apply(plugin = "java")
-  repositories {
-    mavenCentral()
-  }
-  java {
-    withSourcesJar()
-  }
-  tasks {
-    jar {
-      archiveFileName.set("${rootProject.name}-${project.name}.jar")
+    apply(plugin = "java")
+    apply(plugin = "org.sourcegrade.style")
+    version = "1.0.0-SNAPSHOT"
+    repositories {
+        mavenCentral()
     }
-    named<Jar>("sourcesJar") {
-      archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
+    java {
+        withSourcesJar()
     }
-  }
+    tasks {
+        jar {
+            archiveFileName.set("${rootProject.name}-${project.name}.jar")
+        }
+        named<Jar>("sourcesJar") {
+            archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
+        }
+    }
 }
--- grader/build.gradle.kts
+++ grader/build.gradle.kts
@@ -1,8 +1,4 @@
-repositories {
-  maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
-}
-
 dependencies {
-  implementation("org.sourcegrade:jagr-grader-api:0.1.0-SNAPSHOT")
-  implementation(project(":solution"))
+    implementation("org.sourcegrade:jagr-grader-api:0.3")
+    implementation(project(":solution"))
 }
