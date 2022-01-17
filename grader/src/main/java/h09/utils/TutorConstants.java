2e6f44b3be060e09b2f87c958e14c017e3ae3725
Nyanyan <46193012+zentox@users.noreply.github.com>
2022-01-17 18:57:06 +0100

    Remove .* imports

--- grader/src/main/java/h09/utils/TutorConstants.java
+++ grader/src/main/java/h09/utils/TutorConstants.java
@@ -1,7 +1,11 @@
 package h09.utils;
 
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
--- grader/src/main/java/h09/utils/TutorUtils.java
+++ grader/src/main/java/h09/utils/TutorUtils.java
@@ -2,7 +2,12 @@ package h09.utils;
 
 import org.junit.jupiter.api.Assertions;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
