357dba13ae8fb2979ae2e4c48a2a179e9e895140
Alexander Staeding <alexanderstaeding@gmail.com>
2022-02-02 22:10:55 +0100

    New grader structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,77 @@
 plugins {
     java
+    application
     id("org.sourcegrade.style") version "1.2.0"
 }
 
+version = "1.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+val grader: SourceSet by sourceSets.creating {
+    compileClasspath += sourceSets.test.get().compileClasspath
+    runtimeClasspath += output + compileClasspath
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    "graderImplementation"("org.sourcegrade:jagr-launcher:0.4.0-SNAPSHOT")
+    "graderImplementation"("fr.inria.gforge.spoon:spoon-core:10.0.0")
+    "graderImplementation"("org.sourcegrade:docwatcher-api:0.1")
+    "graderImplementation"("org.mockito:mockito-core:4.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+application {
+    mainClass.set("h09.Main")
+}
+
 tasks {
+    val runDir = File("build/run")
+    named<JavaExec>("run") {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+    }
+    test {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        useJUnitPlatform()
+    }
+    val graderTest by creating(Test::class) {
+        group = "verification"
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        testClassesDirs = grader.output.classesDirs
+        classpath = grader.runtimeClasspath
+        useJUnitPlatform()
+    }
+    named("check") {
+        dependsOn(graderTest)
+    }
     create<Jar>("graderJar") {
         group = "build"
         afterEvaluate {
             archiveFileName.set("FOP-2022-H09-${project.version}.jar")
-            from(project(":grader").sourceSets.main.get().allSource)
-            from(project(":solution").sourceSets.main.get().allSource)
-            from(project(":solution").sourceSets.test.get().allSource)
+            from(sourceSets.main.get().allSource)
+            from(sourceSets.test.get().allSource)
+            from(grader.allSource)
         }
     }
-}
-
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "org.sourcegrade.style")
-    version = "1.1.0-SNAPSHOT"
-    repositories {
-        mavenCentral()
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
-    java {
-        withSourcesJar()
-    }
-    tasks {
-        withType<JavaCompile> {
-            options.encoding = "UTF-8"
-        }
-        jar {
-            archiveFileName.set("${rootProject.name}-${project.name}.jar")
-        }
-        named<Jar>("sourcesJar") {
-            archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
-        }
+    jar {
+        enabled = false
     }
 }
